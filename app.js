ce9056fbe55754362ed616a4d90bdc4eb6be49b6
Lane <44852568+LoamySand@users.noreply.github.com>
2023-03-08 18:46:15 -0600

    Unpack application directory

---
+++ app.js
@@ -0,0 +1,59 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+app.locals.basedir=path.join(__dirname, 'S1');
+
+
+// view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.listen(3000);
+
+
+app.get("/", function(req,res) {
+   res.sendFile(__dirname+"/views/index.pug", {title: "login"});
+   res.sendFile()
+ });
+
+app.get('/register', function(req, res, next) {
+   res.render(__dirname+"/views/register.pug", {title: "register"});
+ });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+module.exports = app;
