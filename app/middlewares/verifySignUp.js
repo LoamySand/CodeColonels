dbbb71d417658f164a68eef5f7ccbc113ec2abac
LoamySand <laneeboyd@gmail.com>
2023-04-12 13:13:25 -0500

    adds user to user collection

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -38,16 +38,13 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
 
 checkRolesExisted = (req, res, next) => {
   if (req.body.roles) {
-    for (let i = 0; i < req.body.roles.length; i++) {
-      if (!ROLES.includes(req.body.roles[i])) {
+      if (!ROLES.includes(req.body.roles)) {
         res.status(400).send({
           message: `Failed! Role ${req.body.roles[i]} does not exist!`
         });
         return;
       }
     }
-  }
-
   next();
 };
 
