e696cd2ad0b37cc0b7816b455af7a360981a3f8d
LoamySand <laneeboyd@gmail.com>
2023-04-18 19:40:33 -0500

    edit port listening to dynamic port

--- index.js
+++ index.js
@@ -73,6 +73,6 @@ app.post('/login', async (req, res) => {
     //res.render('home') ???
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('port connected')
 })
