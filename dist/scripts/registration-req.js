5aa7f22613f98ff69fa808de25b2793eb45ac16f
LoamySand <laneeboyd@gmail.com>
2023-04-24 15:19:54 -0500

    merge npm run build and npm start. START APP WITH 'npm start'

--- dist/scripts/registration-req.js
+++ dist/scripts/registration-req.js
@@ -1,24 +1,5 @@
 import { RegistrationReqCollection, UserCollection } from '../dist/models/schema';
-
-$( document ).ready(function() {
-    $('#approve_btn').addEventListener('click', function(){
-        var i = this.parentNode.parentNode.rowIndex;
-        var table = document.getElementById("registrationTable");
-        table.deleteRow(i);
-
-        //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTIONS
-        var row = document.getElementById("request");
-        var cell = row.getElementsByClassName("id");
-        var id = cell[0].innerHTML;
-
-        let requests = RegistrationReqCollection;
-        let user = await requests.findOne({ _id: id });
-        console.log("found user");
-        await UserCollection.insertMany([user]);
-        await requests.deleteOne({ _id: id });
-    }
-}
-export async function approve(r) {
+async function approve(r) {
     var i = r.parentNode.parentNode.rowIndex;
     var table = document.getElementById("registrationTable");
     table.deleteRow(i);
@@ -37,7 +18,7 @@ export async function approve(r) {
     await UserCollection.insertMany([user]);
     await requests.deleteOne({ _id: id });
 }
-export function deny(r) {
+function deny(r) {
     var i = r.parentNode.parentNode.rowIndex;
     var table = document.getElementById("registrationTable");
     table.deleteRow(i);
