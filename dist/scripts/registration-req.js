1ecde5b9bb062ca9a9159298dc19ec8ee8e6055e
LoamySand <laneeboyd@gmail.com>
2023-04-25 10:42:32 -0500

    debug approve/deny btns

--- dist/scripts/registration-req.js
+++ dist/scripts/registration-req.js
@@ -1,27 +1,75 @@
-import { RegistrationReqCollection, UserCollection } from '../dist/models/schema';
-async function approve(r) {
-    var i = r.parentNode.parentNode.rowIndex;
-    var table = document.getElementById("registrationTable");
-    table.deleteRow(i);
-    //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTIONS
-    var row = document.getElementById("request");
-    var cell = row.getElementsByClassName("id");
-    var id = cell[0].innerHTML;
-    //      const id = {
-    //         //_id: document.body._id
+async function approveReq() {
+    // var i = $(this).parentNode.parentNode.rowIndex;
+    // var table = $("#registrationTable") as HTMLTableElement;
+    // var row = table.getElementsByClassName(".request");
+    // var cell = row[0].getElementsByClassName(".id");
+    // var id = cell[0].innerHTML;
     //
-    //          _id = document.getElementById("id").get;
-    //     }
-    let requests = RegistrationReqCollection;
-    let user = await requests.findOne({ _id: id });
-    console.log("found user");
-    await UserCollection.insertMany([user]);
-    await requests.deleteOne({ _id: id });
+    // alert('User clicked on' + i);
+    //
+    //
+    // let user = await RegistrationReqCollection.findOne({ _id: id })
+    // console.log("found user")
+    //
+    // await UserCollection.insertMany([user])
+    //
+    // await RegistrationReqCollection.deleteOne({_id: id })
+    this.style.backgroundColor = "red";
 }
-function deny(r) {
-    var i = r.parentNode.parentNode.rowIndex;
-    var table = document.getElementById("registrationTable");
-    table.deleteRow(i);
-    //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTION
-    //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTIONS ADD TO USER COLLECTION
-}
+$(document).ready(function () {
+    var i = $(this).parentNode.parentNode.rowIndex;
+    document.getElementById("approve_btn").onclick = function () { approveReq; };
+    //$(document).on('click', '#approve_btn', approveReq())
+    //document.getElementsByClassName("approve")[i].addEventListener("click", approveReq);
+    //$('.request').on('click', '#approve_btn', approveReq())
+    // var approveA = $("<td>").attr("href","#").addClass("saveBtn").text("Approve");
+    // var approveBtn = $("#approve_btn").attr("aria-disabled",false).append(approveA);
+    // $(document).find(".actions ul").prepend(approveBtn)
+    //
+    //             var i = this.parentNode.parentNode.rowIndex;
+    //             var table = document.getElementById("registrationTable") as HTMLTableElement;
+    //             table.deleteRow(i);
+    // //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTIONS
+    //             var row = document.getElementById("request");
+    //             var cell = row.getElementsByClassName("id");
+    //             var id = cell[0].innerHTML;
+    //
+    //             let requests = RegistrationReqCollection;
+    //
+    //             let user = await requests.findOne({ _id: id })
+    //             console.log("found user")
+    //
+    //             await UserCollection.insertMany([user])
+    //
+    //             await requests.deleteOne({_id: id })
+});
+export {};
+//
+// async function approve(r){
+// var i = r.parentNode.parentNode.rowIndex;
+// var table = document.getElementById("registrationTable") as HTMLTableElement;
+// table.deleteRow(i);
+// //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTIONS
+//     var row = document.getElementById("request");
+//     var cell = row.getElementsByClassName("id");
+//     var id = cell[0].innerHTML;
+//
+//     let requests = RegistrationReqCollection;
+//
+//     let user = await requests.findOne({ _id: id })
+//     console.log("found user")
+//
+//     await UserCollection.insertMany([user])
+//
+//     await requests.deleteOne({_id: id })
+//
+//
+// },
+//
+// function deny(r){
+// var i = r.parentNode.parentNode.rowIndex;
+// var table = document.getElementById("registrationTable") as HTMLTableElement;
+// table.deleteRow(i);
+// //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTION
+// //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTIONS ADD TO USER COLLECTION
+// }
--- scripts/registration-req.ts
+++ scripts/registration-req.ts
@@ -1,37 +1,77 @@
 import {RegistrationReqCollection, UserCollection, RoleCollection} from '../dist/models/schema';
 //import {RegistrationReqCollection, UserCollection, RoleCollection} from '/models/schema.ts';
 import mongoose from 'mongoose';
-async function approve(r){
-var i = r.parentNode.parentNode.rowIndex;
-var table = document.getElementById("registrationTable") as HTMLTableElement;
-table.deleteRow(i);
-//TODO REMOVE FROM REGISTRATION_REQUEST COLLECTIONS
-    var row = document.getElementById("request");
-    var cell = row.getElementsByClassName("id");
-    var id = cell[0].innerHTML;
-//      const id = {
-//         //_id: document.body._id
+declare var $: any;
+
+async function approveReq(){
+                // var i = $(this).parentNode.parentNode.rowIndex;
+                // var table = $("#registrationTable") as HTMLTableElement;
+                // var row = table.getElementsByClassName(".request");
+                // var cell = row[0].getElementsByClassName(".id");
+                // var id = cell[0].innerHTML;
+                //
+                // alert('User clicked on' + i);
+                //
+                //
+                // let user = await RegistrationReqCollection.findOne({ _id: id })
+                // console.log("found user")
+                //
+                // await UserCollection.insertMany([user])
+                //
+                // await RegistrationReqCollection.deleteOne({_id: id })
+                this.style.backgroundColor = "red";
+        }
+
+
+$( document ).ready(function() {
+        var i = $(this).parentNode.parentNode.rowIndex;
+        //$(document).on('click', '#approve_btn', approveReq())
+        document.getElementsByClassName("approve")[i].addEventListener("click", approveReq);
+
+
+//             var table = document.getElementById("registrationTable") as HTMLTableElement;
+//             table.deleteRow(i);
+// //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTIONS
+//             var row = document.getElementById("request");
+//             var cell = row.getElementsByClassName("id");
+//             var id = cell[0].innerHTML;
 //
-//          _id = document.getElementById("id").get;
-//     }
+//             let requests = RegistrationReqCollection;
+//
+//             let user = await requests.findOne({ _id: id })
+//             console.log("found user")
+//
+//             await UserCollection.insertMany([user])
+//
+//             await requests.deleteOne({_id: id })
 
-    let requests = RegistrationReqCollection;
-
-    let user = await requests.findOne({ _id: id })
-    console.log("found user")
-
-    await UserCollection.insertMany([user])
-
-    await requests.deleteOne({_id: id })
-
-
-}
-
-function deny(r){
-var i = r.parentNode.parentNode.rowIndex;
-var table = document.getElementById("registrationTable") as HTMLTableElement;
-table.deleteRow(i);
-//TODO REMOVE FROM REGISTRATION_REQUEST COLLECTION
-//TODO REMOVE FROM REGISTRATION_REQUEST COLLECTIONS ADD TO USER COLLECTION
-}
-
+})
+//
+// async function approve(r){
+// var i = r.parentNode.parentNode.rowIndex;
+// var table = document.getElementById("registrationTable") as HTMLTableElement;
+// table.deleteRow(i);
+// //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTIONS
+//     var row = document.getElementById("request");
+//     var cell = row.getElementsByClassName("id");
+//     var id = cell[0].innerHTML;
+//
+//     let requests = RegistrationReqCollection;
+//
+//     let user = await requests.findOne({ _id: id })
+//     console.log("found user")
+//
+//     await UserCollection.insertMany([user])
+//
+//     await requests.deleteOne({_id: id })
+//
+//
+// },
+//
+// function deny(r){
+// var i = r.parentNode.parentNode.rowIndex;
+// var table = document.getElementById("registrationTable") as HTMLTableElement;
+// table.deleteRow(i);
+// //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTION
+// //TODO REMOVE FROM REGISTRATION_REQUEST COLLECTIONS ADD TO USER COLLECTION
+// }
